dc959c97e9ae4f3800b2edfdfd7441f30d92ccf4
reokkamy <quinate@kakao.com>
2025-06-16 17:41:05 +0900

    6/16

---
+++ d250616/ch10/prepareStatement.java
@@ -0,0 +1,5 @@
+package d250616.ch10;
+
+public class prepareStatement {
+
+}
---
+++ d250616/ch10/test.sql
@@ -0,0 +1,41 @@
+-- 샘플 시퀀스 만들기. 
+-- 세미 콜론으로 문장 구분을 안했을 경우, 해당 문장 직접 드래그해서, ctrl + enter 로 실행 해주세요.
+create sequence member501_seq start with 1 increment by 1;
+drop sequence member501_seq
+drop table member501;
+--샘플 확인용 테이블 만들기
+create table member501 (
+   id       number primary key,
+   name     varchar2(100),
+   password varchar2(100),
+   email    varchar2(100),
+   reg_date varchar2(50)
+)
+-- 샘플 데이터 하나 넣기. 
+insert into member501 (
+   id,
+   name,
+   password,
+   email,
+   reg_date
+) values ( member501_seq.nextval,
+           '이상용',
+           '1234',
+           'lsy@naver.com',
+           '2025년6월16일12시9분' );
+
+commit;
+
+-- 테이블 조회 
+select *
+  from member501;
+
+-- 한명 회원 조회 sql 
+select *
+  from member501
+ where id = 2;
+
+ -- 검색 조회. 
+select *
+  from member501
+ where name like '%이상용%';
